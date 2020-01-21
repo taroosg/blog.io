9272b4646283b1558fd327ce4ee2d485d411e8c3
taroosg <macchinetta.xyz@gmail.com>
2020-01-21 14:09:25 +0900

    fix ogp

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -97,6 +97,7 @@ const SEO = ({ title, desc, banner, pathname, article }) => (
 
             {/* Twitter Card */}
             <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content={`@${twitter}`} />
             <meta name="twitter:creator" content={twitter} />
             <meta name="twitter:title" content={seo.title} />
             <meta name="twitter:description" content={seo.description} />
