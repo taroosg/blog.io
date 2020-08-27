86bdef888d807f7065ab70354c63af64401f9d4f
taroosg <macchinetta.xyz@gmail.com>
2020-08-27 12:04:12 +0900

    fix unused value

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import Helmet from "react-helmet";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
 const SEO = ({ title, desc, banner, pathname, article }) => (
   <StaticQuery
@@ -29,73 +29,120 @@ const SEO = ({ title, desc, banner, pathname, article }) => (
         title: title || defaultTitle,
         description: defaultDescription || desc,
         image: `${siteUrl}/${banner || defaultBanner}`,
-        url: `${siteUrl}${pathname || '/'}`,
+        url: `${siteUrl}${pathname || "/"}`,
       };
-      const realPrefix = pathPrefix === '/' ? '' : pathPrefix;
-      let schemaOrgJSONLD = [
-        {
-          '@context': 'http://schema.org',
-          '@type': 'WebSite',
-          '@id': siteUrl,
-          url: siteUrl,
-          name: defaultTitle,
-          alternateName: titleAlt || '',
-        },
-      ];
-      if (article) {
-        schemaOrgJSONLD = [
-          {
-            '@context': 'http://schema.org',
-            '@type': 'BlogPosting',
-            '@id': seo.url,
-            url: seo.url,
-            name: title,
-            alternateName: titleAlt || '',
-            headline: title,
-            image: {
-              '@type': 'ImageObject',
-              url: seo.image,
-            },
-            description: seo.description,
-            datePublished: buildTime,
-            dateModified: buildTime,
-            author: {
-              '@type': 'Person',
-              name: author,
-            },
-            publisher: {
-              '@type': 'Organization',
-              name: author,
-              logo: {
-                '@type': 'ImageObject',
-                url: siteUrl + realPrefix + logo,
-              },
-            },
-            isPartOf: siteUrl,
-            mainEntityOfPage: {
-              '@type': 'WebSite',
-              '@id': siteUrl,
-            },
-          },
-        ];
-      }
+      // const realPrefix = pathPrefix === "/" ? "" : pathPrefix;
+      // let schemaOrgJSONLD = [
+      //   {
+      //     "@context": "http://schema.org",
+      //     "@type": "WebSite",
+      //     "@id": siteUrl,
+      //     url: siteUrl,
+      //     name: defaultTitle,
+      //     alternateName: titleAlt || "",
+      //   },
+      // ];
+
+      // const schemaOrgJSONLD = article
+      //   ? [
+      //       {
+      //         "@context": "http://schema.org",
+      //         "@type": "BlogPosting",
+      //         "@id": seo.url,
+      //         url: seo.url,
+      //         name: title,
+      //         alternateName: titleAlt || "",
+      //         headline: title,
+      //         image: {
+      //           "@type": "ImageObject",
+      //           url: seo.image,
+      //         },
+      //         description: seo.description,
+      //         datePublished: buildTime,
+      //         dateModified: buildTime,
+      //         author: {
+      //           "@type": "Person",
+      //           name: author,
+      //         },
+      //         publisher: {
+      //           "@type": "Organization",
+      //           name: author,
+      //           logo: {
+      //             "@type": "ImageObject",
+      //             url: siteUrl + realPrefix + logo,
+      //           },
+      //         },
+      //         isPartOf: siteUrl,
+      //         mainEntityOfPage: {
+      //           "@type": "WebSite",
+      //           "@id": siteUrl,
+      //         },
+      //       },
+      //     ]
+      //   : [
+      //       {
+      //         "@context": "http://schema.org",
+      //         "@type": "WebSite",
+      //         "@id": siteUrl,
+      //         url: siteUrl,
+      //         name: defaultTitle,
+      //         alternateName: titleAlt || "",
+      //       },
+      //     ];
+      // if (article) {
+      //   schemaOrgJSONLD = [
+      //     {
+      //       "@context": "http://schema.org",
+      //       "@type": "BlogPosting",
+      //       "@id": seo.url,
+      //       url: seo.url,
+      //       name: title,
+      //       alternateName: titleAlt || "",
+      //       headline: title,
+      //       image: {
+      //         "@type": "ImageObject",
+      //         url: seo.image,
+      //       },
+      //       description: seo.description,
+      //       datePublished: buildTime,
+      //       dateModified: buildTime,
+      //       author: {
+      //         "@type": "Person",
+      //         name: author,
+      //       },
+      //       publisher: {
+      //         "@type": "Organization",
+      //         name: author,
+      //         logo: {
+      //           "@type": "ImageObject",
+      //           url: siteUrl + realPrefix + logo,
+      //         },
+      //       },
+      //       isPartOf: siteUrl,
+      //       mainEntityOfPage: {
+      //         "@type": "WebSite",
+      //         "@id": siteUrl,
+      //       },
+      //     },
+      //   ];
+      // }
       return (
         // <>
         <Helmet
           title={seo.title}
           meta={[
-            { name: 'twitter:card', content: 'summary_large_image' },
-            { name: 'twitter:site', content: `@${twitter}` },
-            { name: 'twitter:creator', content: `@${twitter}` },
-            { name: 'twitter:title', content: seo.title },
+            { name: "twitter:card", content: "summary_large_image" },
+            { name: "twitter:site", content: `@${twitter}` },
+            { name: "twitter:creator", content: `@${twitter}` },
+            { name: "twitter:title", content: seo.title },
             // { name: 'twitter:description', content: seo.description },
-            { name: 'twitter:image', content: seo.image },
-            { property: 'og:image', content: seo.image },
-            { property: 'og:title', content: seo.title },
-            { property: 'og:type', content: article ? 'article' : 'website' },
-            { property: 'og:description', content: seo.description },
-            { property: 'og:url', content: seo.url },
-            { property: 'fb:admins', content: fb_admin_id }
+            { name: "twitter:image", content: seo.image },
+            { property: "og:image", content: seo.image },
+            { property: "og:title", content: seo.title },
+            { property: "og:type", content: article ? "article" : "website" },
+            { property: "og:description", content: seo.description },
+            { property: "og:url", content: seo.url },
+            { property: "fb:admins", content: fb_admin_id },
           ]}
         />
       );
@@ -103,7 +150,7 @@ const SEO = ({ title, desc, banner, pathname, article }) => (
   />
 );
 
-export default SEO
+export default SEO;
 
 SEO.propTypes = {
   title: PropTypes.string,
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,56 +1,98 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../layouts'
-import card from '../styles/card'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../layouts";
+import card from "../styles/card";
 
 export default ({ data }) => (
   <Layout>
     <div css={card}>
       <h2>Who am I?</h2>
       <ul>
-        <li><p>{data.site.siteMetadata.author}</p></li>
-        <li><p>G's ACADEMY FUKUOKA chief lecturer</p></li>
-        <li><p>From 2018 in Fukuoka</p></li>
-        <li><p>From 2018 in G's ACADEMY</p></li>
-        <li><p>17th November 1987</p></li>
+        <li>
+          <p>{data.site.siteMetadata.author}</p>
+        </li>
+        <li>
+          <p>G's ACADEMY FUKUOKA chief lecturer</p>
+        </li>
+        <li>
+          <p>From 2018 in Fukuoka</p>
+        </li>
+        <li>
+          <p>From 2018 in G's ACADEMY</p>
+        </li>
+        <li>
+          <p>17th November 1987</p>
+        </li>
         <li>
           <p>Interests: </p>
           <ul>
-            <li><p>Javascript</p></li>
-            <li><p>Typescript</p></li>
-            <li><p>React</p></li>
-            <li><p>Firebase</p></li>
-            <li><p>PHP</p></li>
-            <li><p>Laravel</p></li>
-            <li><p>Flutter</p></li>
-            <li><p>Haskell</p></li>
+            <li>
+              <p>Javascript</p>
+            </li>
+            <li>
+              <p>Typescript</p>
+            </li>
+            <li>
+              <p>React</p>
+            </li>
+            <li>
+              <p>Firebase</p>
+            </li>
+            <li>
+              <p>PHP</p>
+            </li>
+            <li>
+              <p>Laravel</p>
+            </li>
+            <li>
+              <p>Flutter</p>
+            </li>
+            <li>
+              <p>Haskell</p>
+            </li>
           </ul>
         </li>
         <li>
           <p>Like: </p>
           <ul>
             <li>
-              <a target="_blank" href="https://www.amazon.jp/hz/wishlist/ls/2W7J0GWI789YV?ref_=wl_share" rel="noopener noreferrer">
+              <a
+                target="_blank"
+                href="https://www.amazon.jp/hz/wishlist/ls/2W7J0GWI789YV?ref_=wl_share"
+                rel="noopener noreferrer"
+              >
                 <p>Islay whiskey</p>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://www.amazon.jp/hz/wishlist/ls/2QRAOKJ20INNP?ref_=wl_share" rel="noopener noreferrer">
+              <a
+                target="_blank"
+                href="https://www.amazon.jp/hz/wishlist/ls/2QRAOKJ20INNP?ref_=wl_share"
+                rel="noopener noreferrer"
+              >
                 <p>Craft gin</p>
               </a>
             </li>
             <li>
-              <a href="https://www.buymeacoffee.com/Bt1hreN" target="_blank" style={{ verticalAlign: 'middle' }}>
-                <img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" style={{ height: 51, width: 217, }} />
+              <a
+                href="https://www.buymeacoffee.com/Bt1hreN"
+                target="_blank"
+                rel="noreferrer"
+                style={{ verticalAlign: "middle" }}
+              >
+                <img
+                  src="https://cdn.buymeacoffee.com/buttons/default-orange.png"
+                  alt="Buy Me A Coffee"
+                  style={{ height: 51, width: 217 }}
+                />
               </a>
             </li>
           </ul>
         </li>
       </ul>
-
     </div>
   </Layout>
-)
+);
 
 export const query = graphql`
   query AboutQuery {
@@ -61,5 +103,4 @@ export const query = graphql`
       }
     }
   }
-`
-
+`;
